096b61419b2c12a236c79a351b227551bf2934f5
Vimal Kumar Thanikachalam <vimaleurakaa@gmail.com>
2021-01-30 18:55:11 +0530

    updated the project

--- app.js
+++ app.js
@@ -1,20 +1,110 @@
-require('dotenv').config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-let port = process.env.PORT || 3000
-const app = express();
+require("dotenv").config();
+const express = require("express"),
+  port = process.env.PORT || 3000,
+  app = express(),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  User = require("./models/user");
 
+//Connecting database
+mongoose.connect(
+  "mongodb+srv://dbUser:dbUser@cluster0.uijgw.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
+
+app.use(
+  require("express-session")({
+    secret: "Any normal Word", //decode or encode session
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+passport.serializeUser(User.serializeUser()); //session encoding
+passport.deserializeUser(User.deserializeUser()); //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", function(req, res){
+//=======================
+//      R O U T E S
+//=======================
+
+app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(port, function() {
-  console.log("Server started on port 3000.");
+app.get("/home", isLoggedIn, (req, res) => {
+  res.render("home");
+  //   { name: req.name }
+});
+
+//Auth Routes
+app.get("/login", (req, res) => {
+  res.render("/" + "#login_new_user");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/" + "#login_failed",
+  }),
+  function (req, res) {}
+);
+
+app.get("/register", (req, res) => {
+  res.render("/" + "#register_new_user");
+});
+
+app.post("/register", (req, res) => {
+  User.register(
+    new User({
+      username: req.body.username,
+      name: req.body.name,
+    }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.render("register");
+      }
+      passport.authenticate("local")(req, res, function () {
+        console.log(req.baseUrl);
+        res.redirect("/" + "#login_new_user");
+      });
+    }
+  );
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/" + "#login");
+}
+
+//Listen On Server
+app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server Started At Port 3000");
+  }
 });
